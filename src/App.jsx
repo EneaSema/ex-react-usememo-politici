f1273df6e67030b01576cf7211e8b3c04d19105c
EneaSema <sema.enea1993@gmail.com>
2025-10-02 17:47:44 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { useState, useEffect } from "react";
+
+export default function App() {
+  const [politicians, setPoliticians] = useState([]);
+
+  useEffect(() => {
+    fetch(`http://localhost:3333/politicians`)
+      .then((resp) => resp.json())
+      .then((data) => setPoliticians(data))
+      .catch((err) => console.error(err));
+  }, []);
+
+  return (
+    <>
+      <h1>EX - Lista di Politici</h1>
+      <div className="lista-politici">
+        {politicians.map((p) => {
+          return (
+            <div className="card" key={p.i}>
+              <img src={p.image} alt={p.name} />
+              <h2>{p.name}</h2>
+              <h3>{p.position}</h3>
+              <p>{p.biography}</p>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
+}
