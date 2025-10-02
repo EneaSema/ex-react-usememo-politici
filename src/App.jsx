ba5fce3f005b76e44dcfbc544008740fec8fa0eb
EneaSema <sema.enea1993@gmail.com>
2025-10-02 17:48:33 +0200

    create structure and resolve milestone 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useMemo } from "react";
 
 export default function App() {
   const [politicians, setPoliticians] = useState([]);
