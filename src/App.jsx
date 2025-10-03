823e9fed171b8dfc5ac1d3719e2d933e6dc9b368
EneaSema <sema.enea1993@gmail.com>
2025-10-03 15:37:31 +0200

    resolve milestone 2 guardadno in parte video soluzione di Hyur

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect, useMemo } from "react";
 
 export default function App() {
   const [politicians, setPoliticians] = useState([]);
+  const [search, setSearch] = useState(``);
 
   useEffect(() => {
     fetch(`http://localhost:3333/politicians`)
@@ -10,11 +11,26 @@ export default function App() {
       .catch((err) => console.error(err));
   }, []);
 
+  const filteredPoliticians = useMemo(() => {
+    return politicians.filter(
+      (p) =>
+        p.name.toLowerCase().includes(search.toLowerCase()) ||
+        p.biography.toLowerCase().includes(search.toLowerCase())
+    );
+  }, [politicians, search]);
+  console.log(filteredPoliticians);
+
   return (
     <>
       <h1>EX - Lista di Politici</h1>
       <div className="lista-politici">
-        {politicians.map((p) => {
+        <input
+          type="text"
+          placeholder="Cerca per nome o biografia"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        {filteredPoliticians.map((p) => {
           return (
             <div className="card" key={p.i}>
               <img src={p.image} alt={p.name} />
