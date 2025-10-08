682a6588f2a78847fc34837bd811cf81c713f84f
EneaSema <sema.enea1993@gmail.com>
2025-10-08 14:46:00 +0200

    resolve milestone 2 guardando video soluzione

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useMemo } from "react";
 
 export default function App() {
+  // variabili reattive
   const [politicians, setPoliticians] = useState([]);
+  const [search, setSearch] = useState("");
 
   useEffect(() => {
     fetch(`http://localhost:3333/politicians`)
@@ -9,16 +11,30 @@ export default function App() {
       .then((data) => setPoliticians(data))
       .catch((err) => console.error(err));
   }, []);
+
+  const politicansFiltered = useMemo(() => {
+    return politicians.filter(
+      (p) =>
+        p.name.toLowerCase().includes(search.toLowerCase()) ||
+        p.biography.toLowerCase().includes(search.toLowerCase())
+    );
+  }, [politicians, search]);
+
   return (
     <>
       <h1>EX - Lista di Politici</h1>
-      {politicians.map((p) => {
-        return (
+      <input
+        type="text"
+        placeholder="Ricerca per nome o biografia"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      {politicansFiltered &&
+        politicansFiltered.map((p) => (
           <div className="card" key={p.id}>
             <div>
               <img src={p.image} alt={p.name} />
             </div>
-
             <div>
               {" "}
               <h3>{p.name}</h3>
@@ -29,8 +45,7 @@ export default function App() {
               <p>{p.biography}</p>
             </div>
           </div>
-        );
-      })}
+        ))}
     </>
   );
 }
